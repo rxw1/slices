9aed468620bee0fd5e8dba50f14162223203329d
René Wilhelm <rene.wilhelm@gmail.com>
2015-08-24 04:31:44 +0200

    Separated routes and middleware

--- src/server/routes/slices.js
+++ src/server/routes/slices.js
@@ -2,17 +2,42 @@ import koa from 'koa';
 import bodyparser from 'koa-bodyparser';
 import compose from 'koa-compose';
 import _ from 'lodash';
-
-import rethinkdbdash from 'rethinkdbdash';
-let r = rethinkdbdash({db: 'slices'});
-
 import Router from 'koa-router';
 let router = new Router();
 
+import { getSlice, sampleSlices } from '../middleware/slices';
+
 router
-  .get('/slices', function* () {
-    const slices = yield r.table('slices').sample(3);
+
+  // get a single slice including all references
+  .get('/api/slices/:sliceID/n', function* (next) {
+    if (!this.params.sliceID) return;
+    const sliceIDs = [parseInt(this.params.sliceID)];
+    const slices = yield getSlice(sliceIDs);
+    _.remove(slices, {sliceID: parseInt(this.params.sliceID)})
     this.body = slices;
+  })
+  // get a single slice including all references
+  .get('/api/slices/:sliceID/refs', function* (next) {
+    if (!this.params.sliceID) return;
+    const sliceIDs = [parseInt(this.params.sliceID)];
+    const slices = yield getSlice(sliceIDs);
+    _.remove(slices, {sliceID: parseInt(this.params.sliceID)})
+    this.body = slices;
+  })
+
+  // get a single slice including all references
+  .get('/api/slices/:sliceID', function* (next) {
+    if (!this.params.sliceID) return;
+    const sliceIDs = [parseInt(this.params.sliceID)];
+    const slices = yield getSlice(sliceIDs);
+    this.body = slices;
+  })
+
+  // get a sample of n slices
+  .get('/api/slices', function* () {
+    const slices = yield sampleSlices();
+    this.body = yield sampleSlices();
   })
 
 const app = koa()
