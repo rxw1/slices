e128c1e0077d380c5680e0ef747ae4b86d497f84
René Wilhelm <rene.wilhelm@gmail.com>
2015-08-24 04:42:39 +0200

    Improved middleware handling

--- src/shared/store/configureStore.js
+++ src/shared/store/configureStore.js
@@ -1,19 +1,29 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import rootReducer from '../reducers';
+import createLogger from 'redux-logger';
 
-const createStoreWithMiddleware = applyMiddleware(
-  thunk,
-  logger
-)(createStore);
 // Redux DevTools store enhancers
 import { devTools, persistState } from 'redux-devtools';
 
+const logger = createLogger({
+	level: 'info',
+	collapsed: true,
+	// predicate: (getState, action) => action.type !== AUTH_REMOVE_TOKEN
+});
+let middleware = [logger, thunk];
+
+// middleware.push(logger);
+
+const createStoreWithMiddleware = compose(
+	applyMiddleware(...middleware),
 	devTools(),
 	// Lets you write ?debug_session=<name> in address bar to persist debug sessions
 	//persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)),
+	createStore
+);
 
 export default function configureStore(initialState) {
-  return createStoreWithMiddleware(rootReducer, initialState);
+	console.log('initial state', initialState);
+	return createStoreWithMiddleware(rootReducer, initialState);
 }
