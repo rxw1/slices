4b18c5e9f27faf5c088e5fd7fc852a4de4932cb1
René Wilhelm <rene.wilhelm@gmail.com>
2015-08-24 04:32:26 +0200

    Cleaned up back end rendering

--- src/server/render.js
+++ src/server/render.js
@@ -1,11 +1,10 @@
-import React             from 'react';
-import { Provider }      from 'react-redux';
-import { Router, Route } from 'react-router';
-import Location          from 'react-router/lib/Location';
-import routes            from '../shared/routes';
-import { fetchSlices }   from '../shared/actions';
-import configureStore    from '../shared/store/configureStore';
-import Root              from '../shared/containers/Root';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router';
+import Location from 'react-router/lib/Location';
+import routes from '../shared/routes';
+import { fetchLanguages } from '../shared/actions/languages';
+import configureStore from '../shared/store/configureStore';
 
 import nunjucks from 'nunjucks';
 nunjucks.configure('src/shared', { autoescape: true });
@@ -14,29 +13,39 @@ export default function render() {
   return function* () {
     const location = new Location(this.request.path, this.request.query);
     const store = configureStore();
-    yield store.dispatch(fetchSlices()); // goes to script tag for hydration
+
+    // yield store.dispatch(fetchLanguages());
 
     this.body = yield new Promise(resolve => {
       Router.run(routes, location, (error, initialRouterState, transition) => {
-
         if (!initialRouterState) {
           return;
         }
 
-        var state = store.getState();
+        function renderRouter() {
+          return (
+            <Router {...initialRouterState}>
+              {routes}
+            </Router>
+          );
+        }
 
         const appString = React.renderToString(
-          <Root initialRouterState={initialRouterState} />
+          <div>
+            <Provider store={store}>
+              {() => renderRouter()}
+            </Provider>
+          </div>
         );
+
+        var state = store.getState();
 
         resolve(nunjucks.render('index.html', {
           appString,
           initialState: JSON.stringify(state),
           env: process.env
         }));
-
       });
     })
-
   }
 }
