47e7ec8ea9058fb1ff4ca61154144db075cf5dbf
René Wilhelm <rene.wilhelm@gmail.com>
2015-08-24 04:29:26 +0200

    Added reducers for referenced and selected slices

--- src/shared/reducers/slices.js
+++ src/shared/reducers/slices.js
@@ -1,4 +1,8 @@
-import { RECEIVE_SLICES } from '../actions';
+import {
+  RECEIVE_SLICES,
+  RECEIVE_REFERENCES,
+  SELECT_SLICE
+} from '../actions/types';
 
 export default function slices(state = [], action) {
   switch (action.type) {
@@ -7,4 +11,24 @@ export default function slices(state = [], action) {
     default:
       return state;
   }
+}
+
+export function references(state = [], action) {
+	switch (action.type) {
+		case RECEIVE_REFERENCES:
+			return [...action.payload];
+		default:
+			return state;
+	}
+}
+
+export function selected(state = {}, action) {
+	switch (action.type) {
+		case SELECT_SLICE:
+			debugger
+			// return [...action.payload]
+			return Object.assign({}, action.payload);
+		default:
+			return state;
+	}
 }
