9528824da3db68cfadca7164fabcffdecad95ff8
René Wilhelm <rene.wilhelm@gmail.com>
2015-08-25 03:38:45 +0200

    Margin on content

--- src/shared/components/Layout/Content.js
+++ src/shared/components/Layout/Content.js
@@ -1,4 +1,12 @@
 import React, { Component, PropTypes } from 'react';
+import StyleSheet from 'react-style';
+var styles = StyleSheet.create({
+  main: {
+    marginTop: '12px',
+    marginRight: '324px',
+    marginLeft: '24px',
+  }
+});
 
 export default class Content extends Component {
   constructor(props) {
@@ -7,7 +15,7 @@ export default class Content extends Component {
 
   render() {
     return (
-      <main className='mdl-layout__content'>
+      <main className='mdl-layout__content' style={styles.main}>
         <div className='page-content'>
           {this.props.children}
         </div>
