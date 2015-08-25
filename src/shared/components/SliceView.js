3d9b5198b19ab473809f970dafd34eaf3838255d
René Wilhelm <rene.wilhelm@gmail.com>
2015-08-25 16:20:36 +0200

    Do import the right stuff

--- src/shared/components/SliceView.js
+++ src/shared/components/SliceView.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import * as actions from '../actions/types';
+import * as actions from '../actions/slices';
 import Slice from '../components/Slice';
 import Fragment from '../components/Fragment';
 import _ from 'lodash';
